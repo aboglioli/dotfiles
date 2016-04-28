58f79e0be9f033f31d385975e2b603e4d7cb8f50
Alan <kiriost.s@gmail.com>
2016-04-28 16:23:18 -0300

    New color in i3 config

--- install.sh
+++ install.sh
@@ -1,59 +1,68 @@
 #!/bin/bash
 SRC_DIR=$PWD
 
-# Remove old files
+# Remove old files and link new files
 rm ~/.bashrc
-rm ~/.conkyrc
-rm ~/.conky/timerc
-rm ~/.dunstrc
-rm ~/.i3/config
-rm ~/.i3status.conf
-rm ~/.rtorrent.rc
-rm ~/.tmux.conf
-rm ~/.vimrc
-rm ~/.xinitrc
-rm ~/.startup_apps
-rm ~/.zshrc
-rm ~/.fonts.conf
-rm ~/.Xresources
-rm ~/.synergy.conf
-rm ~/.background.jpg
-rm ~/.notification.ogg
-
-rm ~/.config/synapse/config.json
-rm ~/.config/terminator/config
-rm -rf ~/.config/ranger
-
-sudo rm /etc/pacman.conf
-sudo rm /etc/yaourtrc
-sudo rm /etc/ssh/ssh_config
-sudo rm /etc/ssh/sshd_config
-
-# Link files to this folder
 ln -s $SRC_DIR/bash/bashrc ~/.bashrc
+
+rm ~/.conkyrc
 ln -s $SRC_DIR/conky/conkyrc ~/.conkyrc
-ln -s $SRC_DIR/conky/timerc ~/.conky/timerc
+
+rm ~/.dunstrc
 ln -s $SRC_DIR/dunst/dunstrc ~/.dunstrc
+
+rm ~/.i3/config
 ln -s $SRC_DIR/i3/config ~/.i3/config
+rm ~/.i3status.conf
 ln -s $SRC_DIR/i3/i3status.conf ~/.i3status.conf
+
+rm ~/.rtorrent.rc
 ln -s $SRC_DIR/rtorrent/rtorrent.rc ~/.rtorrent.rc
+
+rm ~/.tmux.conf
 ln -s $SRC_DIR/tmux/tmux.conf ~/.tmux.conf
+
+rm ~/.vimrc
 ln -s $SRC_DIR/vim/vimrc ~/.vimrc
+
+rm ~/.xinitrc
 ln -s $SRC_DIR/xinitrc ~/.xinitrc
+rm ~/.startup_apps
 ln -s $SRC_DIR/startup_apps ~/.startup_apps
+
+rm ~/.zshrc
 ln -s $SRC_DIR/zsh/zshrc ~/.zshrc
+
+rm ~/.fonts.conf
 ln -s $SRC_DIR/fonts.conf ~/.fonts.conf
+
+rm ~/.Xresources
 ln -s $SRC_DIR/Xresources ~/.Xresources
+
+rm ~/.synergy.conf
 ln -s $SRC_DIR/synergy/synergy.conf ~/.synergy.conf
+
+rm ~/.background.jpg
 ln -s $SRC_DIR/background.jpg ~/.background.jpg
+
+rm ~/.notification.ogg
 ln -s $SRC_DIR/notification.ogg ~/.notification.ogg
 
+rm ~/.config/synapse/config.json
 ln -s $SRC_DIR/synapse/config.json ~/.config/synapse/config.json
+
+rm ~/.config/terminator/config
 ln -s $SRC_DIR/terminator/config ~/.config/terminator/config
+
+rm -rf ~/.config/ranger
 ln -s $SRC_DIR/ranger ~/.config/ranger
 
+sudo rm /etc/pacman.conf
 sudo ln -s $SRC_DIR/pacman/pacman.conf /etc/pacman.conf
+
+sudo rm /etc/ssh/ssh_config
 sudo ln -s $SRC_DIR/ssh/ssh_config /etc/ssh/ssh_config
+sudo rm /etc/ssh/sshd_config
 sudo ln -s $SRC_DIR/ssh/sshd_config /etc/ssh/sshd_config
 
 # Install fonts
---
+++ scripts/clone_monitor.sh
@@ -0,0 +1,1 @@
+xrandr --output $1 --auto --scale-from 1366x768
