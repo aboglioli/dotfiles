42b3c1727319bb42b59691b3002d7f2b28c8c5c3
Alan Boglioli <alan.boglioli@gmail.com>
2020-04-16 01:11:19 -0300

    use term.sh to launch a global terminal

---
+++ scripts/term.sh
@@ -0,0 +1,12 @@
+#!/bin/sh
+
+ID=$(xdo id -N gterm)
+FOCUSED_ID=$(bspc query -N -n focused)
+
+if [ -z "$ID" ]; then
+  st -c gterm -e "tmux"
+elif [ $ID == $FOCUSED_ID  ]; then
+  bspc node $ID -g hidden=on
+else
+  bspc node $ID -d focused -g hidden=off -f
+fi
