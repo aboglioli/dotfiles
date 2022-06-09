20e2e3fdab14ef76bdf2092e4ed46fa1e0528687
Alan Boglioli <alan.boglioli@gmail.com>
2022-06-09 18:56:41 -0300

    split install.sh into config.sh and packages.sh

---
+++ config.sh
@@ -0,0 +1,18 @@
+#!/bin/sh
+
+SRC=~/dotfiles
+
+# ------
+# Config
+# ------
+ln -sf $SRC/background.jpg ~/.background.jpg
+ln -sf $SRC/dunstrc ~/.dunstrc
+ln -sf $SRC/gitconfig ~/.gitconfig
+ln -sf $SRC/zshrc ~/.zshrc
+
+ln -sf $SRC/i3 ~/.config
+ln -sf $SRC/kitty ~/.config
+ln -sf $SRC/nvim ~/.config
+ln -sf $SRC/startup ~/.startup
+ln -sf $SRC/sxhkd ~/.config
+ln -sf $SRC/zathura ~/.config
